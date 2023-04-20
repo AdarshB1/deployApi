cfc44c37a2598434cb180aed693c5a717f58eb5e
Adarsh <Adarsh.b@sankeysolutions.com>
2023-04-20 17:58:44 +0530

    first c

---
+++ CurdApi/NewFolder1/BrandContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CurdApi.NewFolder1
+{
+    public class BrandContext : DbContext 
+    {
+        public BrandContext(DbContextOptions< BrandContext > options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Brand> Brands { get; set; }
+    }
+}
